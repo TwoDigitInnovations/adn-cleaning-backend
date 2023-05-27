46a1ea697771f6553b84b12f72393a2b8cf8b0ff
Chetan7528 <75465248+Chetan7528@users.noreply.github.com>
2023-05-27 18:22:02 +0530

    minor change

--- src/app/controller/service.js
+++ src/app/controller/service.js
@@ -5,47 +5,48 @@ const Booking = mongoose.model("Booking");
 const fs = require("fs");
 
 module.exports = {
-    serviceAvailable: async (req, res) => {
-        try {
+  serviceAvailable: async (req, res) => {
+    try {
+      const zip = Zip.findOne({ code: req.params["zip"] }).lean();
 
-            const zip = Zip.findOne({ code: req.params["zip"] }).lean();
-
-            return response.ok(res, { available: true, message: "available" });
-        } catch (error) {
-            return response.error(res, error);
-        }
-    },
-    getServiceQuestions: async (req, res) => {
-        try {
-            // const service = req.params["service"];
-            const service = "a";
-            fs.readFile(__dirname + `/data/${service}.json`, 'utf-8', (err, data) => {
-                return response.ok(res, { service: JSON.parse(data) });
-            });
-
-        } catch (error) {
-            return response.error(res, error);
-        }
-    },
-    createBooking: async (req, res) => {
-        try {
-            const details = req.body;
-            const booking = new Booking(details);
-            await booking.save();
-            return response.ok(res, { message: "Booking created." });
-        } catch (error) {
-            return response.error(res, error);
-        }
-    },
-    getBookings: async (req, res) => {
-        try {
-            const sd = new Date(req.query["startDate"]);
-            const ed = new Date(req.query["endDate"]);
-            const bookings = await Booking.find({ "slot.date": { $gte: sd, $lte: ed } }).lean();
-            return response.ok(res, { bookings });
-        } catch (error) {
-            return response.error(res, error);
-        }
+      return response.ok(res, { available: true, message: "available" });
+    } catch (error) {
+      return response.error(res, error);
     }
-
-}
+  },
+  getServiceQuestions: async (req, res) => {
+    try {
+      // const service = req.params["service"];
+      const service = "a";
+      fs.readFile(__dirname + `/data/${service}.json`, "utf-8", (err, data) => {
+        return response.ok(res, { service: JSON.parse(data) });
+      });
+    } catch (error) {
+      return response.error(res, error);
+    }
+  },
+  createBooking: async (req, res) => {
+    try {
+      const details = req.body;
+      const booking = new Booking(details);
+      await booking.save();
+      return response.ok(res, { message: "Booking created." });
+    } catch (error) {
+      return response.error(res, error);
+    }
+  },
+  getBookings: async (req, res) => {
+    try {
+      const sd = new Date(req.query["startDate"]);
+      const ed = new Date(req.query["endDate"]);
+      const bookings = await Booking.find({
+        "slot.date": { $gte: sd, $lte: ed },
+      })
+        .populate("booking_for", "username email")
+        .lean();
+      return response.ok(res, { bookings });
+    } catch (error) {
+      return response.error(res, error);
+    }
+  },
+};
--- src/app/model/Booking.js
+++ src/app/model/Booking.js
@@ -1,35 +1,42 @@
-'use strict';
-const mongoose = require('mongoose');
+"use strict";
+const mongoose = require("mongoose");
 
 const bookingSchema = new mongoose.Schema({
-    name: {
-        type: String
-    },
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    },
-    services: {
-        type: Array, "default": []
-    },
-    location: {
-        type: String
-    },
-    slot: {
-        date: { type: Date }, time: { type: String }
-    },
-    card: {
-        type: Object, "default": {}
-    }
+  name: {
+    type: String,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  services: {
+    type: Array,
+    default: [],
+  },
+  location: {
+    type: String,
+  },
+  slot: {
+    date: { type: Date },
+    time: { type: String },
+  },
+  card: {
+    type: Object,
+    default: {},
+  },
+  booking_for: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
 
-bookingSchema.set('toJSON', {
-    getters: true,
-    virtuals: false,
-    transform: (doc, ret, options) => {
-        delete ret.__v;
-        return ret;
-    }
+bookingSchema.set("toJSON", {
+  getters: true,
+  virtuals: false,
+  transform: (doc, ret, options) => {
+    delete ret.__v;
+    return ret;
+  },
 });
 
-module.exports = mongoose.model('Booking', bookingSchema);
+module.exports = mongoose.model("Booking", bookingSchema);
