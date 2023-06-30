a82286bb1d352c6fb46658f7525bc86ac735e8db
Chetan7528 <75465248+Chetan7528@users.noreply.github.com>
2023-06-30 16:11:54 +0530

    minor change

--- src/app/services/mailNotification.js
+++ src/app/services/mailNotification.js
@@ -26,12 +26,12 @@ const sendMail = async (to, subject, html) => {
 
 module.exports = {
   welcomeMail: async (details) => {
-    const html = `<div> \r\n<p>Hello,<\/p>\r\n\r\n<p> Welcome to SwiftGuard. <\/p>\r\n\r\n<p>You recently created a SwiftGuard Account. <\/p>\r\n\r\n<p>Your SwiftGuard Registered Mail is: <b>${details.email} <\/b><\/p>\r\n\r\n<p><\/br>Thanks,<\/p>\r\n\r\n<p><b>The SwiftGuard Account Team<\/b><\/p>\r\n<\/div>`;
-    await sendMail(details.email, "Welcome to SwiftGuard", html);
+    const html = `<div> \r\n<p>Hello,<\/p>\r\n\r\n<p> Welcome to ADN Cleaners service. <\/p>\r\n\r\n<p>You recently created a ADN Cleaners service Account. <\/p>\r\n\r\n<p>Your ADN Cleaners service Registered Mail is: <b>${details.email} <\/b><\/p>\r\n\r\n<p><\/br>Thanks,<\/p>\r\n\r\n<p><b>The ADN Cleaners service Account Team<\/b><\/p>\r\n<\/div>`;
+    await sendMail(details.email, "Welcome to ADN Cleaners service", html);
   },
   sendOTPmail: async ({ email, code }) => {
     try {
-      const html = `<div> \r\n<p>Password Reset Instructions<\/p>\r\n\r\n<p>Your SwiftGuard One-Time password reset code is: ${code}. Enter online when prompted. This passcode will expire in 5 minutes<\/p><\/br>Thank you for updating your password.<\/p>\r\n\r\n<p><b>SwiftGuard<\/b><\/p>\r\n<\/div>`;
+      const html = `<div> \r\n<p>Password Reset Instructions<\/p>\r\n\r\n<p>Your ADN Cleaners service One-Time password reset code is: ${code}. Enter online when prompted. This passcode will expire in 5 minutes<\/p><\/br>Thank you for updating your password.<\/p>\r\n\r\n<p><b>ADN Cleaners service<\/b><\/p>\r\n<\/div>`;
       return await sendMail(email, "Password Reset Instructions", html);
     } catch (err) {
       console.log(err);
@@ -40,7 +40,7 @@ module.exports = {
   },
   passwordChange: async ({ email }) => {
     try {
-      const html = `<div> Your password has been reset, if you didn't update your password, please call us on (.) between 9am - 5pm Monday to Friday. \r\n\r\nSwiftGuard  </div>`;
+      const html = `<div> Your password has been reset, if you didn't update your password, please call us on (.) between 9am - 5pm Monday to Friday. \r\n\r\nADN Cleaners service  </div>`;
       return await sendMail(email, "PASSWORD RESET NOTIFICATION EMAIL", html);
     } catch (err) {
       console.log(err);
